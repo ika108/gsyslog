a21bc090f336730495707eff80b715422dcd0f82
Sven SZEJWACH <git@nimporteou.net>
2014-11-12 17:29:07 +0100

    Syntax and fmt

--- gsyslogd.go
+++ gsyslogd.go
@@ -1,29 +1,29 @@
 package main
 
-import {
-    "fmt"
-    "flag"
-    "github.com/ika108/gsyslog/libs/input"
-    "github.com/ika108/gsyslog/libs/codec"
-    "github.com/ika108/gsyslog/libs/output"
-    "github.com/ika108/gsyslog/libs/filter"
-    "github.com/ika108/gsyslog/libs/queue"
-    "github.com/ika108/gsyslog/libs/message"
-}
+import (
+	"flag"
+	"fmt"
+	"github.com/ika108/gsyslog/libs/codec"
+	"github.com/ika108/gsyslog/libs/filter"
+	"github.com/ika108/gsyslog/libs/input"
+	"github.com/ika108/gsyslog/libs/message"
+	"github.com/ika108/gsyslog/libs/output"
+	"github.com/ika108/gsyslog/libs/queue"
+)
 
-type config {}
+type config struct{}
 
 var configfile string
 var configPtr config
 
-func init () {
+func init() {
 }
 
-func main () {
-    
+func main() {
+
 }
 
-func parseConfig (configFile string){
+func parseConfig(configFile string) {
 	flag.StringVar(&configfile, "configfile", `gsyslogd.conf`, "Define a configfile")
 	flag.Parse()
 	configPtr = parseConfig(configfile)
